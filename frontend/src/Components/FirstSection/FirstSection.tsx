78ffb205128df47ca0f0e5e270f9af39048f7a20
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-16 22:35:46 +0200

    added some content and css

--- frontend/src/Components/FirstSection/FirstSection.tsx
+++ frontend/src/Components/FirstSection/FirstSection.tsx
@@ -3,7 +3,8 @@ import "./FirstSection.scss";
 function FirstSection(): JSX.Element {
     return (
         <div className="FirstSection">
-			first section
+            <h1><span>Hello,</span> I am Alex!</h1>
+            <h2>I make your dream website a reality.</h2>
         </div>
     );
 }
