da48369a14c7b07787721cf5e67b0e7445c774bf
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-24 16:30:27 +0200

    done 4 today

--- frontend/src/Components/1-FirstSection/FirstSection.tsx
+++ frontend/src/Components/1-FirstSection/FirstSection.tsx
@@ -1,3 +1,4 @@
+import { HashLink } from "react-router-hash-link";
 import "./FirstSection.scss";
 
 function FirstSection(): JSX.Element {
@@ -5,6 +6,7 @@ function FirstSection(): JSX.Element {
         <div className="FirstSection" id="/#">
             <h1><span>Hello,</span> I am Alex!</h1>
             <h2>I make your dream website a reality.</h2>
+            <HashLink smooth to='#contact'>Get in touch!</HashLink>
         </div >
     );
 }
