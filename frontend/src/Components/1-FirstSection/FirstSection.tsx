c61fb1a62cbafc8166a6569630796b1de4d54ebd
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-24 11:54:28 +0200

    added logo to navbar

--- frontend/src/Components/1-FirstSection/FirstSection.tsx
+++ frontend/src/Components/1-FirstSection/FirstSection.tsx
@@ -2,7 +2,7 @@ import "./FirstSection.scss";
 
 function FirstSection(): JSX.Element {
     return (
-        <div className="FirstSection">
+        <div className="FirstSection" id="/#">
             <h1><span>Hello,</span> I am Alex!</h1>
             <h2>I make your dream website a reality.</h2>
         </div>
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { HashLink } from 'react-router-hash-link';
 import "./Navbar.scss";
-
+import logo from '../../Assets/Images/logo3.png';
 function Navbar(): JSX.Element {
     return (
         <div className="Navbar">
@@ -8,6 +8,7 @@ function Navbar(): JSX.Element {
                 <li><HashLink smooth to="#about">About</HashLink></li>
                 <li><HashLink smooth to="#services">Services</HashLink></li>
                 <li><HashLink smooth to="#portfolio">Portfolio</HashLink></li>
+                <li><HashLink smooth to="/#"><img src={logo} /></HashLink></li>
                 <li><HashLink smooth to="#recommendations">Recommendations</HashLink></li>
                 <li><HashLink smooth to="#contact">Contact</HashLink></li>
             </ul>
