8b4360e5793f58e798b0be75a9b26e47ecdd7edc
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-26 15:53:04 +0200

    added css 4 buttons

--- frontend/src/Components/1-FirstSection/FirstSection.tsx
+++ frontend/src/Components/1-FirstSection/FirstSection.tsx
@@ -1,12 +1,16 @@
 import { HashLink } from "react-router-hash-link";
 import "./FirstSection.scss";
+import { NavLink } from "react-router-dom";
 
 function FirstSection(): JSX.Element {
     return (
         <div className="FirstSection" id="/#">
             <h1><span>Hello,</span> I am Alex!</h1>
             <h2>I make your dream website a reality.</h2>
-            <HashLink smooth to='#contact'>Get in touch!</HashLink>
+            <div className="links">
+                <NavLink to={'#'}>Download CV</NavLink>
+                <HashLink smooth to='#contact'>Get in touch!</HashLink>
+            </div>
         </div >
     );
 }
